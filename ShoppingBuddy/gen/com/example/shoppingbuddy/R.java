233331219e37a9e6a32ccbc606a6a20ef977dc06
tharindu gayashan <gayasahan.gzn@gmail.com>
2013-03-04 12:12:19 +0530

    Update upto take a photo

--- ShoppingBuddy/gen/com/example/shoppingbuddy/R.java
+++ ShoppingBuddy/gen/com/example/shoppingbuddy/R.java
@@ -12,12 +12,16 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int logo1=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int button1=0x7f070000;
+        public static final int imageView1=0x7f070001;
+        public static final int menu_settings=0x7f070002;
     }
     public static final class layout {
-        public static final int activity_well_come=0x7f030000;
+        public static final int homepage=0x7f030000;
+        public static final int splash=0x7f030001;
     }
     public static final class menu {
         public static final int activity_well_come=0x7f060000;
